61596ba5865fa0965db77e7bddce3e273ade46ff
Khemmie-Ray <atokemmy@gmail.com>
2023-01-26 04:04:55 +0000

    adding mandatory task files

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,10 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   const photo = uploadPhoto();
   const name = createUser();
-  return Promise
-    .then([photo, name])
-    .then((response) => {
-      console.log(`${response[0].body} ${response[1].firstName} ${response[1].lastName}`);
-    })
+  return Promise.all([photo, name])
+    .then((response) => console.log(`${response[0].body} ${response[1].firstName} ${response[1].lastName}`))
     .catch(() => console.log('Signup system offline'));
 }

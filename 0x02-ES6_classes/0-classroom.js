f75dd3c20a688173c72890374fb55e745309a9b6
Khemmie-Ray <atokemmy@gmail.com>
2023-01-27 01:14:44 +0000

    add mandatory task files

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,6 +1,5 @@
-class ClassRoom {
+export default class ClassRoom {
   constructor(maxStudentsSize) {
-    // eslint-disable-next-line no-underscore-dangle
     this._maxStudentsSize = maxStudentsSize;
   }
 }

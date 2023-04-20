0d61e0a37de3fd60e01abb67db36ba0667ce494b
Khemmie-Ray <atokemmy@gmail.com>
2023-04-20 15:29:41 +0000

    add task files

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,8 +1,25 @@
 const fs = require('fs');
 
-function countStudents(path) {
-  if (!fs.existsSync(dataPath)) {
-    throw new Error('Cannot load the database');
+function countStudents(filepath) {
+  const sweStudents = [];
+  const csStudents = [];
+  try {
+    const data = fs.readFileSync(filepath, 'utf-8');
+    const lines = data.toString().split('\n');
+    for (let i = 1; i < lines.length; i += 1) {
+      const field = lines[i].split(',');
+      if (field[3] === 'SWE') {
+        sweStudents.push(field[0]);
+      } else if (field[3] === 'CS') {
+        csStudents.push(field[0]);
+      }
+    }
+    console.log(`Number of students: ${sweStudents.length + csStudents.length}`);
+    console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
+    console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
+  } catch (error) {
+    throw Error('Cannot load the database');
   }
-
 }
+
+module.exports = countStudents;
